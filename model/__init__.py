5bb5dc3f17022b5b6149fc586aa26cf464e224b9
Dev <dchaud26@uwo.ca>
2026-06-09 23:44:40 -0400

    fixed a bug

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,2 @@
-from _vqvae import VQVAE
-from .swin.swin_video import SwinVideoEncoder
+from ._vqvae import VQVAE
+from .swin.swin_video import SwinVideoEncoder, SwinVideoDecoder, SwinVideoAutoencoder
